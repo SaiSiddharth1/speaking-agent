cfa53a0aa6ff7e71dcbe9dd7c6a8397290cee3c9
SaiSiddharth <siddharth.kalavala@gmail.com>
2026-05-06 23:42:37 +0530

    feat: add RecordingScreen and useAudioRecorder with mic permission and state UI

--- mobile/navigation/AppNavigator.tsx
+++ mobile/navigation/AppNavigator.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import VoiceScreen from '../screens/VoiceScreen';
+import RecordingScreen from '../screens/RecordingScreen';
 
 export type RootStackParamList = {
   Voice: undefined;
+  Recording: undefined;
   // Future screens go here:
   // Home: undefined;
   // Transcript: { text: string };
@@ -24,6 +26,7 @@ export default function AppNavigator() {
         }}
       >
         <Stack.Screen name="Voice" component={VoiceScreen} />
+        <Stack.Screen name="Recording" component={RecordingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
